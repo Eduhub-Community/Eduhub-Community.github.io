0e02592969873b99a213db12b76239171a2f9fa6
SahityaRoy <SahityaRoy@users.noreply.github.com>
2023-06-26 18:46:10 +0000

    Prettified Code!

--- chatBot/script.js
+++ chatBot/script.js
@@ -1,94 +1,90 @@
 // MESSAGE INPUT
-const textarea = document.querySelector('.chatbox-message-input')
-const chatboxForm = document.querySelector('.chatbox-message-form')
+const textarea = document.querySelector(".chatbox-message-input");
+const chatboxForm = document.querySelector(".chatbox-message-form");
 
-textarea.addEventListener('input', function () {
-	let line = textarea.value.split('\n').length
+textarea.addEventListener("input", function () {
+  let line = textarea.value.split("\n").length;
 
-	if(textarea.rows < 6 || line < 6) {
-		textarea.rows = line
-	}
+  if (textarea.rows < 6 || line < 6) {
+    textarea.rows = line;
+  }
 
-	if(textarea.rows > 1) {
-		chatboxForm.style.alignItems = 'flex-end'
-	} else {
-		chatboxForm.style.alignItems = 'center'
-	}
-})
-
-
+  if (textarea.rows > 1) {
+    chatboxForm.style.alignItems = "flex-end";
+  } else {
+    chatboxForm.style.alignItems = "center";
+  }
+});
 
 // TOGGLE CHATBOX
-const chatboxToggle = document.querySelector('.chatbox-toggle')
-const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
+const chatboxToggle = document.querySelector(".chatbox-toggle");
+const chatboxMessage = document.querySelector(".chatbox-message-wrapper");
 
-chatboxToggle.addEventListener('click', function () {
-	chatboxMessage.classList.toggle('show')
-})
-
-
+chatboxToggle.addEventListener("click", function () {
+  chatboxMessage.classList.toggle("show");
+});
 
 // DROPDOWN TOGGLE
-const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle')
-const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu')
+const dropdownToggle = document.querySelector(
+  ".chatbox-message-dropdown-toggle"
+);
+const dropdownMenu = document.querySelector(".chatbox-message-dropdown-menu");
 
-dropdownToggle.addEventListener('click', function () {
-	dropdownMenu.classList.toggle('show')
-})
+dropdownToggle.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("show");
+});
 
-document.addEventListener('click', function (e) {
-	if(!e.target.matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
-		dropdownMenu.classList.remove('show')
-	}
-})
-
-
-
-
-
-
+document.addEventListener("click", function (e) {
+  if (
+    !e.target.matches(".chatbox-message-dropdown, .chatbox-message-dropdown *")
+  ) {
+    dropdownMenu.classList.remove("show");
+  }
+});
 
 // CHATBOX MESSAGE
-const chatboxMessageWrapper = document.querySelector('.chatbox-message-content')
-const chatboxNoMessage = document.querySelector('.chatbox-message-no-message')
+const chatboxMessageWrapper = document.querySelector(
+  ".chatbox-message-content"
+);
+const chatboxNoMessage = document.querySelector(".chatbox-message-no-message");
 
-chatboxForm.addEventListener('submit', function (e) {
-	e.preventDefault()
+chatboxForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-	if(isValid(textarea.value)) {
-		writeMessage()
-		setTimeout(autoReply, 1000)
-	}
-})
-
-
+  if (isValid(textarea.value)) {
+    writeMessage();
+    setTimeout(autoReply, 1000);
+  }
+});
 
 function addZero(num) {
-	return num < 10 ? '0'+num : num
+  return num < 10 ? "0" + num : num;
 }
 
 function writeMessage() {
-	const today = new Date()
-	let message = `
+  const today = new Date();
+  let message = `
 		<div class="chatbox-message-item sent">
 			<span class="chatbox-message-item-text">
-				${textarea.value.trim().replace(/\n/g, '<br>\n')}
+				${textarea.value.trim().replace(/\n/g, "<br>\n")}
 			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(
+    today.getMinutes()
+  )}</span>
 		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	chatboxForm.style.alignItems = 'center'
-	textarea.rows = 1
-	textarea.focus()
-	textarea.value = ''
-	chatboxNoMessage.style.display = 'none'
-	scrollBottom()
+	`;
+  chatboxMessageWrapper.insertAdjacentHTML("beforeend", message);
+  chatboxForm.style.alignItems = "center";
+  textarea.rows = 1;
+  textarea.focus();
+  textarea.value = "";
+  chatboxNoMessage.style.display = "none";
+  scrollBottom();
 }
 
 function autoReply() {
-	const today = new Date()
-	let message = `
+  const today = new Date();
+  let message = `
 		<div class="chatbox-message-item received">
 			<span class="chatbox-message-item-text">
 				Hello,
@@ -96,20 +92,22 @@ function autoReply() {
 			<span class="chatbox-message-item-text">
 				so how can I help you ?
 			</span>
-			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+			<span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(
+    today.getMinutes()
+  )}</span>
 		</div>
-	`
-	chatboxMessageWrapper.insertAdjacentHTML('beforeend', message)
-	scrollBottom()
+	`;
+  chatboxMessageWrapper.insertAdjacentHTML("beforeend", message);
+  scrollBottom();
 }
 
 function scrollBottom() {
-	chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight)
+  chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight);
 }
 
 function isValid(value) {
-	let text = value.replace(/\n/g, '')
-	text = text.replace(/\s/g, '')
+  let text = value.replace(/\n/g, "");
+  text = text.replace(/\s/g, "");
 
-	return text.length > 0
+  return text.length > 0;
 }
